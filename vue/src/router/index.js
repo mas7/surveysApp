5031adbfe55e2b703f37dc45ae9ede8d98c2a2bd
Mohamed Sorour <mas.qa77@gmail.com>
2022-04-16 07:00:48 +0200

    Added surveys view

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Dashboard from "../views/dashboard.vue";
 import Login from "../views/login.vue";
 import Register from "../views/register.vue";
+import Dashboard from "../views/dashboard.vue";
+import Surveys from "../views/surveys.vue";
+import DefaultLayout from "../components/DefaultLayout.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Dashboard",
-    component: Dashboard,
+    redirect: "/dashboard",
+    component: DefaultLayout,
+    children: [
+      { path: "/dashboard", name: "Dashboard", component: Dashboard },
+      { path: "/surveys", name: "Surveys", component: Surveys },
+    ],
   },
   {
     path: "/login",
